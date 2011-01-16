18df502330c95c3b8b5cbe45cf3a05f3a05f841d
unknown <John@.(none)>
2011-01-16 17:09:32 -0500

    Added much more extensive documentation in the form of comments

--- Submodule/MyForm.cpp
+++ Submodule/MyForm.cpp
@@ -8,16 +8,31 @@
 #include "Utilities/Memaddr.h"
 
 // local declaration of module handle defined in submodule.cpp
+// this handle is needed to extract resources embedded in this module (e.g. dialog templates)
 extern HMODULE hModule;
 
 // TESForm virtual method overrides
 MyForm::~MyForm()
 {
     _VMESSAGE("Destroying '%s'/%p:%p @ <%p>",GetEditorID(),GetFormType(),formID,this);
+    /*
+        Clean up any dynamically allocated members here.
+    */
 }
 bool MyForm::LoadForm(TESFile& file)
 {
     _VMESSAGE("Loading '%s'/%p:%p @ <%p>",GetEditorID(),GetFormType(),formID,this);
+    /*
+        Load form data from a file record.
+        This method must be overwritten (TESForm::LoadForm does nothing), unless this
+        form class derives from some child of TESForm that has already overwritten it.
+
+        Chunks are usually saved in a fixed order, but all vanilla implementations of LoadForm
+        allow them to be loaded in a more flexible order - hence the loop and switch over chunk
+        types.
+
+    */
+
     file.InitializeFormFromRecord(*this);
 
     char buffer[0x200];
@@ -27,35 +42,36 @@ bool MyForm::LoadForm(TESFile& file)
         switch (Swap32(chunktype))
         {
 
-        // editor id chunk      
+        // editor id      
         case 'EDID':
-            // already processed by loading routine 
-            memset(buffer,0,sizeof(buffer));
-            file.GetChunkData(buffer,sizeof(buffer)-1);
+            memset(buffer,0,sizeof(buffer));    // initialize buffer to zero, good practice
+            file.GetChunkData(buffer,sizeof(buffer)-1); // load chunk into buffer
             _VMESSAGE("EDID chunk: '%s'",buffer);
             SetEditorID(buffer);
             break;   
 
         // name
         case 'FULL':            
-            TESFullName::LoadComponent(*this,file);
+            TESFullName::LoadComponent(*this,file); // use base class method
             _VMESSAGE("FULL chunk: '%s'",name.c_str());
             break;
 
         // description
         case 'DESC':            
-            TESDescription::LoadComponent(*this,file);
+            TESDescription::LoadComponent(*this,file); // use base class method
             _VMESSAGE("DESC chunk: '%s'",GetDescription(this,Swap32('DESC')));
             break;
 
         // icon
         case 'ICON':            
-            TESIcon::LoadComponent(*this,file);
+            TESIcon::LoadComponent(*this,file); // use base class method
             _VMESSAGE("ICON chunk: path '%s'",texturePath.c_str());
             break;
 
         // extra data
         case 'DATA':
+            // load all simple BaseFormComponents using LoadGenericComponents()
+            // the extraData value, specific to this form class, is stored at the end of the DATA chunk
             TESForm::LoadGenericComponents(file,&extraData,sizeof(extraData));
             _VMESSAGE("DATA chunk: value %i weight %f extraData %i",goldValue,weight,extraData);
             break;
@@ -75,49 +91,79 @@ bool MyForm::LoadForm(TESFile& file)
 void MyForm::SaveFormChunks()
 {
     _VMESSAGE("Saving '%s'/%p:%p @ <%p>",GetEditorID(),GetFormType(),formID,this);
-    InitializeFormRecord();
+    /*
+        Save form data to a file record.
+        This method must be overwritten (TESForm::SaveFormChunks does nothing), unless this
+        form class derives from some child of TESForm that has already overwritten it.
+    */
 
-    TESFullName::SaveComponent();
-    TESDescription::SaveComponent();
-    TESIcon::SaveComponent(Swap32('ICON'));
+    // initialize the global Form Record memory buffer, to which chunks are written by all 'Save' methods
+    // InitializeFormRecord() also automatically saves the EDID chunk
+    InitializeFormRecord(); 
+
+    // Save component chunks
+    TESFullName::SaveComponent(); // use base class method, saves in a FULL chunk
+    TESDescription::SaveComponent(); // use base class method, saves in a DESC chunk
+    TESIcon::SaveComponent(Swap32('ICON')); // use base class method, saves in an ICON chunk
+
+    // save all simple BaseFormComponents using SaveGenericComponents() in a DATA chunk
+    // the extraData value, specific to this form class, is saved at the end of the DATA chunk
     SaveGenericComponents(&extraData,sizeof(extraData));
 
+    // close the global form record buffer & write out to disk
     FinalizeFormRecord();
 }
 UInt8 MyForm::GetFormType()
 {
+    /*
+        Return the form type code.  This method *MUST* be overwritten
+        It must return the type code assigned when this form type was registered with ExtendedForm.
+        It may, in exceptional circumstances, also return zero as an 'invalid type' code.
+    */
     return extendedForm.FormType();
 }
 void MyForm::CopyFrom(TESForm& form)
 {
     _VMESSAGE("Copying '%s'/%p:%p @ <%p> ONTO '%s'/%p:%p @ <%p> ",
         form.GetEditorID(),form.GetFormType(),form.formID,&form,GetEditorID(),GetFormType(),formID,this);
+    /*
+        Copy member values.  This method *MUST* be overwritten (TESForm::CopyFrom does nothing).
+        This function does not copy 'index' values like formID, editorID, mgefCode, etc. *unless*
+        one of the forms involved is flagged as temporary.
+    */
 
     MyForm* source = dynamic_cast<MyForm*>(&form);
     if (!source) return;    // source has wrong polymorphic type
 
-    CopyGenericComponentsFrom(form); // copy generic components
-
-    extraData = source->extraData;
+    CopyGenericComponentsFrom(form); // copy all BaseFormComponent properties
+    extraData = source->extraData; // copy extraData, which is specific this form class
 
 }
 bool MyForm::CompareTo(TESForm& compareTo)
 {
     _VMESSAGE("Comparing '%s'/%p:%p @ <%p> TO '%s'/%p:%p @ <%p> ",
         GetEditorID(),GetFormType(),formID,this,compareTo.GetEditorID(),compareTo.GetFormType(),compareTo.formID,&compareTo);
+    /*
+        Return false if forms are identical, including polymorphic type.
+        This method *MUST* be overwritten (TESForm::CompareTo does nothing).
+        This function apparently *does* compare 'index' values like formID, editorID, mgefCode, etc.
+    */
 
     MyForm* source = dynamic_cast<MyForm*>(&compareTo);
     if (!source) true;    // source has wrong polymorphic type
 
-    if (CompareGenericComponentsTo(compareTo)) return true; // mismatch in generic components
+    if (CompareGenericComponentsTo(compareTo)) return true; // compare all BaseFormComponent properties
+    if (extraData != source->extraData) return true;    // compare extraData, which is specific this form class
 
-    if (extraData != source->extraData) return true;    // extraData doesn't match
-
-    return false;
+    return false; // forms are identical
 }
 #ifndef OBLIVION
 bool MyForm::DialogMessageCallback(HWND dialog, UINT uMsg, WPARAM wParam, LPARAM lParam, LRESULT& result)
 {
+    /*
+        Handle any dialog messages specific to this form class' dialog window
+        Returns true if message handled.
+    */
     if (uMsg == WM_COMMAND && LOWORD(wParam) == IDC_GETTYPEINFO)
     {
         // user has clicked on the 'Get Form Type Info' button
@@ -127,42 +173,65 @@ bool MyForm::DialogMessageCallback(HWND dialog, UINT uMsg, WPARAM wParam, LPARAM
          MessageBox(dialog,buffer,"Extended Form Type Information",0);
          return true;
     }
+    // call TESFormIDListView::DialogMessageCallback to process messages general to all TESFormIDListViews
+    // this includes messages related to the form list control, the context menu, and the controls 
+    // associated with all BaseFormComponents.
     return TESFormIDListView::DialogMessageCallback(dialog,uMsg,wParam,lParam,result);
 }
 void MyForm::SetInDialog(HWND dialog)
 {
+    /*
+        Set control values to match members        
+    */
+
     HWND control = 0;
 
     if (dialogHandle == 0)
     {
+        // the global dialog handle hasn't been updated, meaning that this is the
+        // first time this method has been called since the dialog was opened
+        // use this block to initialize the dialog - populate combos, lists, etc.
         _DMESSAGE("Initializing Dialog");
         
-        control = GetDlgItem(dialog,IDC_EXTRADATA);
-        TESComboBox::PopulateWithActorValues(control,true,true);
+        control = GetDlgItem(dialog,IDC_EXTRADATA); // get handle of extraData combobox
+        TESComboBox::PopulateWithActorValues(control,true,true); // populate combo with a list of actor values
 
-        dialogHandle = dialog;
+        dialogHandle = dialog;  // set the global dialog handle
     }
+
+    // call TESFormIDListView::SetInDialog to update the controls associated with all BaseFormComponents
     TESFormIDListView::SetInDialog(dialog);
 
+    // update the extraData combo selection to match the current value of extraData
     control = GetDlgItem(dialog,IDC_EXTRADATA);
     TESComboBox::SetCurSelByData(control,(void*)extraData);
 }
 void MyForm::GetFromDialog(HWND dialog)
 {
+    /*
+        Set member values to match controls        
+    */
+
     HWND control = 0;
 
+    // call TESFormIDListView::GetFromDialog to update the properties associated with all BaseFormComponents
     TESFormIDListView::GetFromDialog(dialog);
 
+    // set the value of extraData from the current combo selection
     control = GetDlgItem(dialog,IDC_EXTRADATA);
     extraData = (UInt32)TESComboBox::GetCurSelData(control);
 }
 void MyForm::CleanupDialog(HWND dialog)
 {
-    dialogHandle = 0;
+    /*
+        Perform cleanup when the dialog is closed        
+    */
+
+    dialogHandle = 0;   // clear the global dialog handle, since the window is now closed
 }
 #endif
 
-// bitmask of virtual methods that need to be manually copied from vanilla vtbls
+// bitmask of virtual methods that need to be manually copied from vanilla vtbls (see notes in Constructor)
 #ifdef OBLIVION
     const UInt32 TESForm_NoUseMethods[2] = { 0x86048400, 0x001E1FC7 };
     const UInt32 BaseFormComponent_NoUseMethods[1] = {0x00};
@@ -182,11 +251,31 @@ MyForm::MyForm()
 : TESFormIDListView(), TESFullName(), TESDescription(),TESIcon(),TESWeightForm(),TESValueForm(), extraData(0)
 {
     _VMESSAGE("Constructing '%s'/%p:%p @ <%p>",GetEditorID(),GetFormType(),formID,this);
+    /*
+        Initialize a new instance of this form class   
+        Note the initializers following the function name above - these call the default contstructors
+        for each of the base classes, and initialize the value of the extraData to zero.
+    */
 
-    // use form type assigned during extended form registration
+    // set the form type assigned during extended form registration
     formType = extendedForm.FormType(); 
 
-    // first time through - set up vtbl
+    /*
+            Many of the Oblivion classes defined in COEF are incomplete.  In particular, while
+        the number of virtual methods is always correct, the signatures of many are still unknown.
+        For compatibility reasons, COEF marks such methods with the '_NOUSE' macro and does not
+        import them.  This means that when the compiler automatically generates a virtual method
+        table (vtbl) for each class, some of the entries are blank.       
+            When dealing with instances created by the Game/CS this makes little differece, since 
+        those objects use the Game/CS version of the vtbls.  If an instance is created by a COEF-
+        based plugin, however, it will use the COEF version of the vtbl.  This can lead to serious 
+        errors.
+            To address this, the COEF vtbl must be patched at run time by copying the addresses of
+        the NOUSE_ methods from the vanilla vtbl to fill in the blanks.  It needs to be done only
+        once per new form class, the first time that class' constructor is called.
+            A polymorphic class has one vtbl per base class.  In this example, that means six 
+        vtbls.
+    */
     if (static bool runonce = true)
     {
         // patch up TESForm vtbl, including it's BaseFormComponent section        
@@ -233,18 +322,24 @@ MyForm::MyForm()
     }
 }
 // COEF ExtendedForm component
-ExtendedForm MyForm::extendedForm(SOLUTIONNAME,"MyForm","My Form",MyForm::CreateMyForm);  
-TESForm* MyForm::CreateMyForm() { return new MyForm; }
+// This global object is used to register the form class with the ExtendedForm COEF component
+ExtendedForm MyForm::extendedForm(SOLUTIONNAME,MYFORM_CLASSNAME,MYFORM_CLASSNAME,MyForm::CreateMyForm);  
+TESForm* MyForm::CreateMyForm() { return new MyForm; } // method used by ExtendedForm to create new instances of this class
 
 // CS dialog management 
 #ifndef OBLIVION
-UInt32 MyForm::kMenuIdentifier = 0xCC00;
-HWND  MyForm::dialogHandle = 0;
+UInt32 MyForm::kMenuIdentifier = 0xCC00;    // unique identifier for new menu item (see InitializeMyForm())
+HWND  MyForm::dialogHandle = 0; // handle of open dialog window, if any
 LRESULT MyForm_CSMenuHook(WPARAM wparam, LPARAM lparam)
 {
+    /*
+        CSMainWindow_WMCommand event handler
+        Peeks at WM_COMMAND messages sent to the main CS window
+        Returns zero if message has been handled
+    */
     if (LOWORD(wparam) == MyForm::kMenuIdentifier)
     {
-        // WM_COMMAND messagesent by added menu item
+        // WM_COMMAND messagesent by new menu item
         MyForm::OpenDialog();
         return 0;
     }
@@ -252,12 +347,22 @@ LRESULT MyForm_CSMenuHook(WPARAM wparam, LPARAM lparam)
 }
 void MyForm::OpenDialog()
 {
-    if (dialogHandle == 0)
+    /*
+        Open CS dialog for editing forms of this type
+    */
+    if (dialogHandle == 0)  // check if dialog is already open
     {
+         // define a 'FormEditParam' object, passed as an argument to the DialogProc
         TESDialog::FormEditParam param;
         param.formType = extendedForm.FormType();
         param.form = 0;
-        HWND handle = CreateDialogParam(hModule,MAKEINTRESOURCE(IDD_MYFORMDLG),TESDialog::csHandle,&TESFormIDListView::DlgProc,(LPARAM)&param);
+        // open a modeless dialog for editing forms of this type
+        // TESFormIDListView::DlgProc is used as the DialogProc
+        HWND handle = CreateDialogParam(    hModule, // handle of module in which dialog template is embedded
+                                            MAKEINTRESOURCE(IDD_MYFORMDLG), // dialog template identifier
+                                            TESDialog::csHandle, // handle of parent window
+                                            &TESFormIDListView::DlgProc, // pointer to DialogProc
+                                            (LPARAM)&param); // application specific parameter
         _DMESSAGE("Opened Dialog w/ handle %p",handle);
     }
 }
@@ -266,9 +371,12 @@ void MyForm::OpenDialog()
 // global initialization function
 void MyForm::InitializeMyForm()
 {
-    _MESSAGE("Initializing MyForm ...");
+    /*
+        Perform one-time initialization required for this form class
+    */
+    _MESSAGE("Initializing " MYFORM_CLASSNAME " ...");
 
-    // register form type
+    // register form type with the ExtendedForm COEF component
     extendedForm.Register(MYFORM_SHORTNAME);
     
     #ifndef OBLIVION
@@ -276,20 +384,23 @@ void MyForm::InitializeMyForm()
     // generate a (hopefully) unqiue menu identifier from form type
     kMenuIdentifier += extendedForm.FormType();
 
-    // insert menu item
+    // insert new item into CS main menu
     HMENU menu = GetMenu(TESDialog::csHandle); // get main CS menu handle
     menu = GetSubMenu(menu,3);              // get 'World' submenu handle
     MENUITEMINFO iteminfo;      
     iteminfo.cbSize = sizeof(iteminfo);        
     iteminfo.fMask = MIIM_ID | MIIM_FTYPE | MIIM_STRING;
     iteminfo.fType = MFT_STRING;
-    char menulabel[] = SOLUTIONNAME " Forms ...";
+    char menulabel[] = SOLUTIONNAME " " MYFORM_CLASSNAME " ...";
     iteminfo.dwTypeData = menulabel;
     iteminfo.cch = sizeof(menulabel);
     iteminfo.wID = kMenuIdentifier;
     InsertMenuItem(menu,0,true,&iteminfo); // Insert new entry at top of submenu
 
-    // register event handler
+    // register event handler with the EventManager COEF component
+    // the CSMainWindow_WMCommand occurs when WM_COMMAND messages are sent to the main CS window
+    // This includes menu selections by the user, so this event handler will be able to
+    // determine when the user clicks on the newly added menu item.
     EventManager::RegisterEventCallback(EventManager::CSMainWindow_WMCommand,&MyForm_CSMenuHook);
 
     #endif
--- Submodule/MyForm.h
+++ Submodule/MyForm.h
@@ -1,5 +1,36 @@
 /* 
     Example of a new Form class
+
+    See Components/ExtendedForm.h for general details on new form classes.  The comments
+    here are specific to this example.
+
+    This new form type is derived from multiple BaseFormComponents, which give it the 'standard'
+    Name, Description, Icon, Gold value, and Weight properties.  The Game/CS code for these classes
+    is frequently used implicitly (for example, the CopyFrom() method defined in MyForm.cpp calls
+    the TESForm::CopyGenericComponentsFrom(), which in turn calls the CopyFrom methods for each
+    individual BaseFormComponent to copy their properties.
+
+    The most important base class is TESFormIDListView.  This base derives from TESForm, so it provides
+    the necessary inheritance to that class.  It also has additional methods targeted to a specific
+    style of CS dialog - one where all forms of this type are edited in a single dialog, a la EffectSettings,
+    TESRaces, etc.
+
+    This form class takes up several limited resources, which can potentially cause conflicts.
+    -   Form Type Code
+        As discussed in ExtendedForm.h, this new form class is automatically assigned a form type 
+        code upon registration.
+
+    -   Short Name / Plugin Name / Class Name:
+        As discussed in ExtendedForm.h, this new form class *must* define a 4-character short name 
+        for use in serialization, and this string must be *unique* among all new form classes from 
+        all plugins.
+    
+    -   Menu Identifier:
+        Each item in a menu must have a unique identifier, used to identify it when it is selected 
+        by the user.  There are quite a few possible identifiers (any 16 bit number > ~0x4000 works),
+        but the possibility for conflict with other plugins that insert menu or toolbar items remains.
+        The identifier for the new menu item introduced to open the MyForms dialog is determined by 
+        adding the assigned form type to a base value 0xCC00.
 */
 #pragma once
 
@@ -8,9 +39,12 @@
 #include "API/TESForms/BaseFormComponent.h" // additonal form components
 #include "Components/ExtendedForm.h"
 
-#define MYFORM_SHORTNAME "MYFM" 
+// Macros for short name and class name, which must be unique among all plugins, and just this plugin, respectively
+#define MYFORM_SHORTNAME "MYFM"
+#define MYFORM_CLASSNAME "MyForm" 
 
-class MyForm :  public TESFormIDListView, // a child of TESForm, defines methods for editing all forms of this type in a single CS dialog
+// MyForm - new form class definition
+class MyForm :  public TESFormIDListView,
                 public TESFullName,     // gives this form a standard "name" property
                 public TESDescription,  // gives this form a standard "description" property 
                 public TESIcon,         // gives this form an icon
